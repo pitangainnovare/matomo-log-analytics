993cf6ca67c154c9b4a24f15927f5ff3b7701d39
Rafael JPD <rafael.pezzuto@gmail.com>
2021-01-31 17:05:45 -0300

    Reorganiza comandos de acordo com os novos módulos

--- update_available_logs.py
+++ update_available_logs.py
@@ -1,137 +1,39 @@
 # coding=utf-8
 import argparse
-import datetime
 import logging
 import os
-import re
 import shutil
 
-from sqlalchemy.orm.exc import NoResultFound
-from libs.lib_database import get_db_session
-from models.declarative import LogFile, DateStatus
+from libs.lib_database import update_available_log_files, update_date_status, get_recent_log_files
+from libs.lib_file_name import FILE_GUNZIPPED_LOG_EXTENSION
 
-COPY_FILES_LIMIT = int(os.environ.get('COPY_FILES_LIMIT', 6))
-COLLECTION = os.environ.get('COLLECTION', 'scl')
+
 DIRS_USAGE_LOGS = os.environ.get('DIRS_USAGE_LOGS', '/logs_hiperion,/logs_node03').split(',')
 DIR_WORKING_LOGS = os.environ.get('DIR_WORKING_LOGS', '.')
 LOG_FILE_DATABASE_STRING = os.environ.get('LOG_FILE_DATABASE_STRING', 'mysql://user:pass@localhost:3306/logs_files')
+
+COPY_FILES_LIMIT = int(os.environ.get('COPY_FILES_LIMIT', 10))
+COLLECTION = os.environ.get('COLLECTION', 'scl')
+
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
 
-REGEX_DATE = r'\d{4}-\d{2}-\d{2}'
 
+def copy_available_log_files(database_uri, collection, dir_working_logs, copy_files_limit):
+    current_files = os.listdir(dir_working_logs)
 
-def _extract_log_server_name(full_path):
-    if 'node03' in full_path:
-        return 'node03'
-    elif 'hiperion' in full_path:
-        if 'apache' in full_path:
-            return 'hiperion-apache'
-        if 'varnish' in full_path:
-            return 'hiperion-varnish'
+    recent_files = get_recent_log_files(database_uri, collection, ignore_loaded=True)
 
-
-def _extract_log_date(full_path):
-    matched_date = re.search(REGEX_DATE, full_path)
-    if matched_date:
-        return matched_date.group()
-
-
-def _extract_name(server, date):
-    file_name = server + '-' + date
-    return file_name
-
-
-def copy_files():
-    current_files = os.listdir(DIR_WORKING_LOGS)
-
-    db_session = get_db_session(LOG_FILE_DATABASE_STRING)
-
-    recent_files = db_session.query(LogFile).filter(LogFile.status == 'queue').order_by(LogFile.created_at).limit(COPY_FILES_LIMIT)
-    for rf in recent_files:
-        rf_name_gz = rf.name + '.gz'
+    for rf in sorted(recent_files.limit(copy_files_limit), key=lambda x: x.name):
+        rf_name_gz = rf.name + FILE_GUNZIPPED_LOG_EXTENSION
         if rf_name_gz not in current_files:
             source = rf.full_path
-            target = os.path.join(DIR_WORKING_LOGS, rf_name_gz)
+            target = os.path.join(dir_working_logs, rf_name_gz)
 
             if os.path.exists(target):
                 logging.warning('Log file %s exists' % target)
             else:
                 logging.info('Copying file "%s" to "%s"' % (source, target))
                 shutil.copy(source, target)
-
-
-def update_log_file_table():
-    db_session = get_db_session(LOG_FILE_DATABASE_STRING)
-
-    for path in DIRS_USAGE_LOGS:
-        for root, dirs, files in os.walk(path):
-            for name in files:
-                file = os.path.join(root, name)
-                server = _extract_log_server_name(file)
-                date = _extract_log_date(file)
-
-                try:
-                    existing_log_file = db_session.query(LogFile).filter(LogFile.full_path == file).one()
-                except NoResultFound:
-                    lf = LogFile()
-                    lf.full_path = file
-                    lf.created_at = datetime.datetime.fromtimestamp(os.stat(file).st_ctime)
-                    lf.size = os.stat(file).st_size
-                    lf.server = server
-                    lf.date = date
-                    lf.name = _extract_name(server, date)
-                    lf.status = 'queue'
-                    lf.collection = COLLECTION
-
-                    db_session.add(lf)
-                    db_session.commit()
-                    logging.info('LogFile row created: (%s, %s)' % (lf.full_path, lf.created_at.strftime('%y-%m-%d %H:%M:%S')))
-
-
-def _compute_date_status(status_list):
-    status_sum = 0
-    for s in status_list:
-        if s == 'loaded':
-            status_sum += 1
-
-    if status_sum == 2:
-        return 'loaded'
-    elif status_sum == 1:
-        return 'partial'
-
-    return 'queue'
-
-
-def update_date_status_table():
-    db_session = get_db_session(LOG_FILE_DATABASE_STRING)
-
-    try:
-        lfdate_to_status_files = {}
-        for lf in db_session.query(LogFile).filter(LogFile.collection == COLLECTION):
-            if lf.date not in lfdate_to_status_files:
-                lfdate_to_status_files[lf.date] = []
-            lfdate_to_status_files[lf.date].append(lf.status)
-
-        for key in lfdate_to_status_files:
-            new_status = _compute_date_status(lfdate_to_status_files[key])
-
-            try:
-                existing_date_status = db_session.query(DateStatus).filter(DateStatus.date == key).one()
-                if new_status != existing_date_status.status:
-                    if existing_date_status.status in ['queue', 'partial']:
-                        existing_date_status.status = new_status
-
-            except NoResultFound:
-                new_ds = DateStatus()
-                new_ds.status = new_status
-                new_ds.date = key
-                new_ds.collection = COLLECTION
-
-                db_session.add(new_ds)
-    except NoResultFound:
-        logging.error('There are no log files registered in the table log_file')
-
-    db_session.commit()
 
 
 if __name__ == '__main__':
@@ -156,12 +58,11 @@ if __name__ == '__main__':
 
     if 'update_log_file' in params.exec_mode:
         logging.info('Updating table log_file')
-        update_log_file_table()
+        update_available_log_files(LOG_FILE_DATABASE_STRING, DIRS_USAGE_LOGS, COLLECTION)
 
     if 'copy_logs' in params.exec_mode:
-        logging.info('Copying possible new log files to the local working directory %s' % DIR_WORKING_LOGS)
-        copy_files()
+        copy_available_log_files(LOG_FILE_DATABASE_STRING, COLLECTION, DIR_WORKING_LOGS, COPY_FILES_LIMIT)
 
     if 'date_status' in params.exec_mode:
         logging.info('Updating table date_status')
-        update_date_status_table()
+        update_date_status(LOG_FILE_DATABASE_STRING, COLLECTION)
