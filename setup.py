0ba84cab7b20205693e1b051a13f1d2416523d27
Rafael JPD <rafael.pezzuto@gmail.com>
2021-09-08 18:34:03 -0300

    atualiza versão de aplicação em setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name="scielo-matomo-manager",
-    version='0.1',
+    version='0.1.1',
     description="The SciELO Matomo Manager",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
