be55829da51cb2ee433baf0cdecf92d936a9d9e7
Rafael JPD <rafael.pezzuto@gmail.com>
2022-04-26 13:59:15 -0300

    update_avaliable_logs Adequa passagem de parâmetros e faz melhorias em nomes de variáveis

--- proc/update_available_logs.py
+++ proc/update_available_logs.py
@@ -19,6 +19,11 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
+        '--collection',
+        required=True,
+    )
+
+    parser.add_argument(
         '--execution_mode',
         action='append',
         help='Modos de execução',
@@ -31,19 +36,19 @@ def main():
     parser.add_argument(
         '--dir_usage_logs',
         help='Diretório com arquivos de log',
-        default=DIR_USAGE_LOGS,
+        required=True,
     )
 
     params = parser.parse_args()
 
-    logging.basicConfig(level=LOGGING_LEVEL,
+    logging.basicConfig(level=LOGLEVEL,
                         format='[%(asctime)s] %(levelname)s %(message)s',
                         datefmt='%d/%b/%Y %H:%M:%S')
 
     if 'update_table_log_file' in params.execution_mode:
         logging.info('Updating table log_file with possible new logs in %s' % params.dir_usage_logs)
-        lib_database.update_available_log_files(SESSION_FACTORY(), params.dir_usage_logs, COLLECTION)
+        lib_database.update_available_log_files(SESSION_FACTORY(), params.dir_usage_logs, params.collection)
 
     if 'update_table_date_status' in params.execution_mode:
         logging.info('Updating table date_status')
-        lib_database.update_date_status(SESSION_FACTORY(), COLLECTION)
+        lib_database.update_date_status(SESSION_FACTORY(), params.collection)
