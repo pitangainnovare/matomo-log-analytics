92ec36cffbcc4f0e47515ba1460ae8c7ade4e61c
Rafael JPD <rafael.pezzuto@gmail.com>
2022-07-06 15:38:11 -0300

    Altera para 2 arquivos esperados para coleção nbr e datas > 2022-05-08

--- libs/lib_status.py
+++ libs/lib_status.py
@@ -28,6 +28,7 @@ COLLECTION_TO_EXPECTED_DAILY_STATUS_SUM = {
     'esp': 1,
     'mex': 1,
     'nbr': {
+        'after_2022_05_08': 2,
         'after_2022_04_05': 3,
         'before_2022_04_06': 2,
     },
@@ -62,7 +63,9 @@ def compute_date_status(logfile_status_list, collection, date=None):
         else:
             expected_status_sum = COLLECTION_TO_EXPECTED_DAILY_STATUS_SUM.get(collection).get('before_2021_05_25')
     elif collection == 'nbr':
-        if date > datetime.datetime.strptime('2022-04-05', '%Y-%m-%d').date():
+        if date > datetime.datetime.strptime('2022-05-08', '%Y-%m-%d').date():
+            expected_status_sum = COLLECTION_TO_EXPECTED_DAILY_STATUS_SUM.get(collection).get('after_2022_05_08')
+        elif date > datetime.datetime.strptime('2022-04-05', '%Y-%m-%d').date():
             expected_status_sum = COLLECTION_TO_EXPECTED_DAILY_STATUS_SUM.get(collection).get('after_2022_04_05')
         else:
             expected_status_sum = COLLECTION_TO_EXPECTED_DAILY_STATUS_SUM.get(collection).get('before_2022_04_06')
